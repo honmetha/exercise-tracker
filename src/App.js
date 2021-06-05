015db18fa82ed42b211214ce0a659b31bc3336f4
Hon <dennis.mejen@gmail.com>
2021-06-05 17:32:16 +0700

    [Added] Part 1: Project Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          Signin
+        </Route>
+        <Route path="/sign-up">Signup</Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
