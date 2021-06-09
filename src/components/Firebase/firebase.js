7707fc25ca204fdda7ea40c9b3c671d9e5037dfa
Hon <dennis.mejen@gmail.com>
2021-06-09 23:54:08 +0700

    [Added] Part 2: Firebase Setup

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,34 @@
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+var config = {
+  apiKey: "AIzaSyCPjYnqkUONz0hXlhyt534BTKel_w_8jEw",
+  authDomain: "workout-tracker-13609.firebaseapp.com",
+  projectId: "workout-tracker-13609",
+  storageBucket: "workout-tracker-13609.appspot.com",
+  messagingSenderId: "326217281458",
+  appId: "1:326217281458:web:946faa16116346f7fafeab",
+  measurementId: "G-E1DGYX9T91",
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
+    // this.db = app.database(); Later add database
+  }
+
+  /*** Authentication  ***/
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+}
+
+export default Firebase;
