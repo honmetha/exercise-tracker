8238a72cbc780c4a5b3df2a048503fee8a0a0973
Hon <dennis.mejen@gmail.com>
2021-06-13 15:33:45 +0700

    [Added] Part 2: Signing Up a User

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -9,6 +10,7 @@ import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
+
 import useStyles from "../config/theme-signinup";
 import Copyright from "../components/Copyright";
 
@@ -31,8 +33,6 @@ export default function SignInSide() {
   const handleSubmit = (e) => {};
 
   const isValid = user.email === "" || user.password === "";
-
-  console.log(`user`, user);
 
   return (
     <Grid container component="main" className={classes.root}>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { withFirebase } from "../components/Firebase";
+import { withRouter } from "react-router-dom";
+
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -9,13 +12,15 @@ import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
+
 import useStyles from "../config/theme-signinup";
 import Copyright from "../components/Copyright";
 
-export default function SignInSide() {
+function SignUp(props) {
   const classes = useStyles();
   const initialUser = {
     id: null,
+    name: "",
     email: "",
     password: "",
     error: null,
@@ -28,12 +33,20 @@ export default function SignInSide() {
     setUser({ ...user, [name]: value });
   };
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    props.firebase.auth
+      .createUserWithEmailAndPassword(user.email, user.password)
+      // Later add user also to database
+      .then((authUser) => {
+        setUser(initialUser);
+        props.history.push("/dashboard");
+      })
+      .catch((error) => {
+        setUser({ ...user, error: error.message });
+      });
+  };
 
-  const isValid =
-    user.email === "" || user.password === "" || user.username === "";
-
-  console.log(`user`, user);
+  const isValid = user.name === "" || user.email === "" || user.password === "";
 
   return (
     <Grid container component="main" className={classes.root}>
@@ -45,7 +58,7 @@ export default function SignInSide() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Sign Up
           </Typography>
           <form
             className={classes.form}
@@ -57,12 +70,12 @@ export default function SignInSide() {
               margin="normal"
               required
               fullWidth
-              id="username"
-              label="User Name"
-              name="username"
-              autoComplete="username"
+              id="name"
+              label="Name"
+              name="name"
               autoFocus
-              onchange={handleChange}
+              value={user.name}
+              onChange={handleChange}
             />
             <TextField
               variant="outlined"
@@ -74,7 +87,7 @@ export default function SignInSide() {
               name="email"
               autoComplete="email"
               autoFocus
-              onchange={handleChange}
+              onChange={handleChange}
             />
             <TextField
               variant="outlined"
@@ -86,8 +99,11 @@ export default function SignInSide() {
               type="password"
               id="password"
               autoComplete="current-password"
-              onchange={handleChange}
+              onChange={handleChange}
             />
+            <Typography className={classes.error}>
+              {user.error ? user.error : ""}
+            </Typography>
             <Button
               type="submit"
               fullWidth
@@ -97,18 +113,11 @@ export default function SignInSide() {
               onClick={handleSubmit}
               disabled={isValid}
             >
-              Sign Up
+              Sign up
             </Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
               <Grid item>
-                <Link href="/" variant="body2">
-                  {"Already have an account?"}
-                </Link>
+                <Link to="/">{"Already have an account? Sign In"}</Link>
               </Grid>
             </Grid>
             <Box mt={5}>
@@ -120,3 +129,5 @@ export default function SignInSide() {
     </Grid>
   );
 }
+
+export default withRouter(withFirebase(SignUp));
