750b27fc3e5548a46c6f42f189e8d136672dfd45
Hon <dennis.mejen@gmail.com>
2021-06-19 15:36:54 +0700

    [Added] Part 3: Calendar Head

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,5 +1,6 @@
+import * as React from "react";
 import { withAuthentication } from "../Session";
-// import moment from "moment";
+import moment from "moment";
 
 import Grid from "@material-ui/core/Grid";
 
@@ -9,10 +10,36 @@ import CalendarHead from "./calendar-head";
 function Calendar(props) {
   // const { firebase, authUser } = props;
 
+  /* HOOKS */
+  const [dateObject, setdateObject] = React.useState(moment());
+  const [showMonthTable, setShowMonthTable] = React.useState(false);
+
+  /* CALENDAR HEAD */
+  const allMonths = moment.months();
+  const currentMonth = () => dateObject.format("MMMM");
+  const currentYear = () => dateObject.format("YYYY");
+
+  const setMonth = (month) => {
+    let monthNo = allMonths.indexOf(month);
+    let newDateObject = Object.assign({}, dateObject);
+    newDateObject = moment(dateObject).set("month", monthNo);
+    setdateObject(newDateObject);
+    setShowMonthTable(false);
+  };
+
+  const toggleMonthSelect = () => setShowMonthTable(!showMonthTable);
+
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} md={8} lg={9}>
-        <CalendarHead />
+        <CalendarHead
+          allMonths={allMonths}
+          currentMonth={currentMonth}
+          currentYear={currentYear}
+          setMonth={setMonth}
+          showMonthTable={showMonthTable}
+          toggleMonthSelect={toggleMonthSelect}
+        />
         <CalendarBody />
       </Grid>
     </Grid>
